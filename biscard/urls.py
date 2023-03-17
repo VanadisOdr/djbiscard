f8c5ea88ade0c47ff135147972e261630e8e395a
VanadisOdr <oleggnedov@mail.ru>
2023-03-17 05:35:22 +0300

    TODO css, but finally ready to prod

--- biscard/urls.py
+++ biscard/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, re_path
+from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path('', views.index),
-    path('info', views.info),
-    path('about', views.about, kwargs={"name":"Oleg", "age": 21, "job": "МИАЦ"}),
-    re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
-    re_path(r"^user/(?P<name>\D+)", views.user),
-    re_path(r"^user", views.user),
+    path("", views.home)
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,36 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("<h2>Главная</h2>")
-
-
-def about(request, name, age, job): #О пользователе инфа
-    return HttpResponse(f"""
-            <h2>О пользователе</h2>
-            <p>Имя: {name}</p>
-            <p>Возраст: {age}</p>
-            <p>Работаю в: {job}</p>
-    """)
-
-def info(request):
-    host = request.META["HTTP_HOST"]  # получаем адрес сервера
-    user_agent = request.META["HTTP_USER_AGENT"]  # получаем данные бразера
-    user_ip = request.META["REMOTE_ADDR"] #айпи клиента
-    path = request.path  # получаем запрошенный путь
-
-    return HttpResponse(f"""
-        <p>Host: {host}</p>
-        <p>Path: {path}</p>
-        <p>User-agent: {user_agent}</p>
-        <p>User-ip: {user_ip}</p>
-        
-    """)
-def user(request,  name="Undefined", age=0): #Передаю дефолтные значения
-    return HttpResponse(f"""
-            <h2>Страница пользователя</h2>
-            <p>Имя: {name}</p>
-            <p>Возраст: {age}</p>
-        
-    """)
-
+def home(request):
+    return render(request, 'home.html')
